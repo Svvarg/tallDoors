b0c6d1342a742af40f3b5f3df1adeac02e0d2d9d
Tektor <thaddel707@gmail.com>
2013-08-09 19:55:55 +0200

    rendering now works for all directions

--- tektor/minecraft/talldoors/renderer/RenderEntranceDoor1.java
+++ tektor/minecraft/talldoors/renderer/RenderEntranceDoor1.java
@@ -87,11 +87,62 @@ public class RenderEntranceDoor1 extends Render {
 		}
 		else if(orientation == 2)
 		{
+			if (left) {
+
+				if ( pos == 1) {
+					x = x -0.7f;
+				} else {
+					z = z - 0.7f;
+				}
 			
+			}
+			GL11.glTranslatef((float) x + 0.5f, (float) y + 1.5f, (float) z
+					+1.3f - 0.8f * pos);
+			GL11.glRotatef(180f, 0f, 0f, 1f);
+			int i = 0;
+			if (left) {
+				GL11.glRotatef(180f, 0f, 1f, 0f);
+				i = -1;
+
+			} else
+				i = 1;
+			GL11.glRotatef(0f + i * pos * 90f, 0f,
+					1f, 0f);
 		}
 		else if(orientation == 3)
 		{
-			
+			if (left) {
+
+				if (pos == 1) {
+					x = x - 0.8f;
+					z = z - 1.5f;
+				} else {
+					x = x - 0.9f;
+					
+				}
+				
+			}
+			else
+			{
+				if (pos == 1) {
+					x = x - 0.8f;
+					z = z - 0.8f;
+				} else {
+					x = x - 1.6f;
+				}
+			}
+			GL11.glTranslatef((float) x + 1.3f, (float) y + 1.5f, (float) z
+					+0.5f + 0.8f * pos);
+			GL11.glRotatef(180f, 0f, 0f, 1f);
+			int i = 0;
+			if (left) {
+				GL11.glRotatef(180f, 0f, 1f, 0f);
+				i = -1;
+
+			} else
+				i = 1;
+			GL11.glRotatef(90f + i * pos * 90f, 0f,
+					1f, 0f);
 		}
 		GL11.glScalef(1f, 1f, 1f);
 		this.func_110776_a(this.func_110775_a(entity));
